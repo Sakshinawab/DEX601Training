1d8b6636aa7c6d3f58f01784ed23a267e84cf125
Sakshi Nawab <sakshi.nawab@ge.com>
2019-12-04 14:12:30 +0530

    Exercise 4-2 PanicButton in tab

--- force-app/Exercises/main/default/aura/StudentDetail/StudentDetailController.js
+++ force-app/Exercises/main/default/aura/StudentDetail/StudentDetailController.js
@@ -5,5 +5,13 @@
 		var contactId = event.getParam('contactId');
 		component.set('v.contactId',contactId);
 		component.find('recordLoader').reloadRecord();
+		},
+
+		onGoToRecord : function(component, event, helper){
+			var evt = $A.get('event.force:navigateToSObject');
+			evt.setParams({recordId : component.get('v.contactId'),
+							slideDevName : 'detail'	
+		});
+		evt.fire();
 		}
 })
