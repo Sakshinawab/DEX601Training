448a93601db82f84eb0777adea4446c3924c44ac
Sakshi Nawab <sakshi.nawab@ge.com>
2019-12-05 15:32:45 +0530

    Exercise 5-2 Completed

--- force-app/Exercises/main/default/aura/AwInstructors/AwInstructorsController.js
+++ force-app/Exercises/main/default/aura/AwInstructors/AwInstructorsController.js
@@ -5,6 +5,10 @@
 		component.set('v.viewMode', 'students');
 		} else {
 		component.set('v.viewMode', 'certifications');
-		}
+			var key = selectionName.split(',');
+			component.set('v.certificationId', key[0]);
+			console.log('I want to learn'+key[0] + key[1]);
+			component.set('v.certificationLabel', key[1]);
+			}
 		}
 })
